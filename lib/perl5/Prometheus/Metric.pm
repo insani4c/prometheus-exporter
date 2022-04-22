f4bca07e214759a1aff0e2cfd6727031e4843b28
Johnny Morano <insani4c@code-monkey.ninja>
2022-04-22 06:01:15 +0200

    change: added Histogram support and required_arguments

--- lib/perl5/Prometheus/Metric.pm
+++ lib/perl5/Prometheus/Metric.pm
@@ -5,10 +5,17 @@ use warnings;
 use base qw/Prometheus/;
 use Prometheus::Metric::Gauge;
 use Prometheus::Metric::Counter;
+use Prometheus::Metric::Histogram;
 use Data::Dumper;
+
+use constant DEFAULT_NAME => "unconfigured_metric";
+use constant DEFAULT_TYPE => "gauge";
+use constant DEFAULT_DESC => "An unconfigured metric";
 
 sub new {
     my($class, $args) = @_;
+
+    $class->required_arguments($args);
 
     if($args->{type} eq 'gauge'){
         $class = 'Prometheus::Metric::Gauge';
@@ -16,14 +23,25 @@ sub new {
     elsif($args->{type} eq 'counter'){
         $class = 'Prometheus::Metric::Counter';
     }
-
-    $args    = {} unless defined $args;
+    elsif($args->{type} eq 'histogram'){
+        $class = 'Prometheus::Metric::Histogram';
+    }
 
     my $self = $class->new($args);
+
     $self->add_factory();
 
     return $self;
 }
+
+sub required_arguments {
+    $_[1] = {} unless defined $_[1];
+
+    $_[1]->{type} //= DEFAULT_TYPE;
+    $_[1]->{desc} //= DEFAULT_DESC;
+    $_[1]->{name} //= DEFAULT_NAME;
+}
+
 
 sub value {
     my ($self, $v) = @_; 
