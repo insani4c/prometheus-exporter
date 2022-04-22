465fe885b9ad1140088c5ca45ab7033302fed82b
Johnny Morano <insani4c@code-monkey.ninja>
2022-04-22 12:57:59 +0200

    change: examples/testd.pl
    - use threads shared variables for counter and histogram
    - cleanup code
    - listen only on localhost

--- examples/testd.pl
+++ examples/testd.pl
@@ -2,6 +2,8 @@
 use strict;
 use warnings;
 use Log::Log4perl;
+use threads;
+use threads::shared;
 
 use lib './lib/perl5';
 use Prometheus::Exporter;
@@ -17,26 +19,40 @@ my $logini = q{
 Log::Log4perl->init( \$logini );
 $Log::Log4perl::caller_depth = 1;
 
-my $exporter = Prometheus::Exporter->new({listen_port => 9090, max_threads => 5});
+# tagged with :shared to share them amongst threads
+my $test_counter :shared  = 0;
+my %histo_buckets :shared = ( "0.3" => 0, "0.6" => 0, "1.2" => 0, "+Inf" => 0 );
+
+my $exporter = Prometheus::Exporter->new({
+    listen_port => 9090, 
+    listen_addr => "127.0.0.1", 
+    max_threads => 5,
+});
+
 $exporter->register_metrics({
     test_metric        => {type => "gauge",     desc => "A test metric"},
     test_metric_labels => {type => "gauge",     desc => "A test metric", labels => ["code=42", "code=99"]},
     test_counter       => {type => "counter",   desc => "A test metric"},
-    test_histogram     => {type => "histogram", desc => "A test metric", buckets => ['0.3', '0.6', '1.2']},
+    test_histogram     => {type => "histogram", buckets => ['0.3', '0.6', '1.2', '+Inf']},
 });
 
 $exporter->register_collector(sub {
     my $timeout = int(rand(5));
     sleep $timeout;
 
-    $exporter->{metrics_factory}->factory("test_metric")->value(rand(100));
-    $exporter->{metrics_factory}->factory("test_metric_labels")->value([rand(42), rand(99)]);
-    $exporter->{metrics_factory}->factory("test_counter")->value(int(rand(78)));
+    $exporter->get_metric("test_metric")->value(rand(100));
+    $exporter->get_metric("test_metric_labels")->value([rand(42), rand(99)]);
 
-    my $histo_buckets = { "0.3" => rand(500), "0.6" => rand(300), "1.2" => rand(100) };
-    my $histo_sum = rand(1000);
-    my $histo_count = $histo_sum + rand(10000);
-    $exporter->{metrics_factory}->factory("test_histogram")->value($histo_buckets, $histo_sum, $histo_count);
+    $test_counter += int(rand(20));
+    $exporter->get_metric("test_counter")->value($test_counter);
+
+    $histo_buckets{"0.3"}  += rand(20);
+    $histo_buckets{"0.6"}  += $histo_buckets{"0.3"} + rand(20);
+    $histo_buckets{"1.2"}  += $histo_buckets{"0.6"} + rand(20);
+    $histo_buckets{"+Inf"} += $histo_buckets{"1.2"} + rand(20);
+    my $histo_sum = 2.0 * $histo_buckets{"+Inf"};
+    my $histo_count = $histo_buckets{"+Inf"};
+    $exporter->get_metric("test_histogram")->value(\%histo_buckets, $histo_sum, $histo_count);
 });
 
 $exporter->run;
